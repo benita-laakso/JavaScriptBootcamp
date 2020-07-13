13529da21b9ebe96820193a3c1fff5637f9fb892
Benita Laakso <benitalaaksofi@yahoo.com>
2020-07-13 11:14:03 +0300

    Error solving. Block loding mixed content http://puzzle.mead.io. Fixed the issue by removing http in code ->///puzzle.mead.io.This will automatikally match the protocall of the website

--- hangman/Scripts/requests.js
+++ hangman/Scripts/requests.js
@@ -4,7 +4,7 @@
 //the function gets called with two arguments;
 //resolve and reject are functios
 const getPuzzle = async(wordCount) => {
-   const response = await fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
+   const response = await fetch(`//puzzle.mead.io/puzzle?wordCount=${wordCount}`)
       if(response.status === 200){
          const data = await response.json()
          return data.puzzle
@@ -19,7 +19,7 @@ const getCurrentCountry = async () =>{
 }
 
 const getCountry = async (countryCode) => {
-      const response = await fetch('https://restcountries.eu/rest/v2/all')
+      const response = await fetch('//restcountries.eu/rest/v2/all')
    
       if (response.status === 200){
          const data = await response.json()
@@ -30,7 +30,7 @@ const getCountry = async (countryCode) => {
 }
 
 const getLokation = async () => {
-   const response = await fetch('https://ipinfo.io/json?token=debfd83366f742')
+   const response = await fetch('//ipinfo.io/json?token=debfd83366f742')
       if (response.status === 200){
          return response.json()
       }else{
